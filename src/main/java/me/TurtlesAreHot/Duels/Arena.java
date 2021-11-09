26e98e98b3a0f249f971fcde0672add4c1257bd6
TurtlesAreHot <37197684+Twocoolguy@users.noreply.github.com>
2021-11-09 13:54:13 -0500

    Update Arena.java

--- src/main/java/me/TurtlesAreHot/Duels/Arena.java
+++ src/main/java/me/TurtlesAreHot/Duels/Arena.java
@@ -8,21 +8,27 @@ import org.bukkit.Location;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Arena {
     private Location pos1;
     private Location pos2;
     private String name;
+    private List<String> commandsAfter;
 
-    public Arena(Location pos1, Location pos2, String name) {
+    public Arena(Location pos1, Location pos2, String name, List<String> commandsAfter) {
         this.pos1 = pos1;
         this.pos2 = pos2;
         this.name = name;
+        this.commandsAfter = commandsAfter;
     }
 
     public Location getPos1() { return this.pos1; }
 
     public Location getPos2() { return this.pos2; }
+
+    public List<String> getCommandsAfter() { return this.commandsAfter; }
 
     public boolean exists() {
         File arenaConfigFile = new File(Main.getDataDirectory(), "/arenas/" + name + ".yml");
@@ -50,9 +56,11 @@ public class Arena {
         } catch (IOException | InvalidConfigurationException e) {
             e.printStackTrace();
         }
+        List<String> def = new ArrayList<>();
         arenaConfig.addDefault("player1-location", getPos1());
         arenaConfig.addDefault("player2-location", getPos2());
         arenaConfig.addDefault("name", name);
+        arenaConfig.addDefault("commands-after", def);
         arenaConfig.options().copyDefaults(true);
         try {
             arenaConfig.save(arenaConfigFile);
