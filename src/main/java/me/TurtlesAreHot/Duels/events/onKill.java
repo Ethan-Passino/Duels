823c728ca1f734e65df61ca4cbc6979ff1fa0858
TurtlesAreHot <37197684+Twocoolguy@users.noreply.github.com>
2021-11-09 13:14:53 -0500

    Update onKill.java

--- src/main/java/me/TurtlesAreHot/Duels/events/onKill.java
+++ src/main/java/me/TurtlesAreHot/Duels/events/onKill.java
@@ -3,6 +3,7 @@ package me.TurtlesAreHot.Duels.events;
 import me.TurtlesAreHot.Duels.Duel;
 import me.TurtlesAreHot.Duels.Main;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -13,22 +14,19 @@ import java.util.UUID;
 public class onKill implements Listener {
     @EventHandler
     public void onDeath(PlayerDeathEvent event) {
-        if(event.getEntity().getKiller() != null) {
-            List<UUID> duelPlayers = Main.getDuelPlayers();
-            for(UUID p : duelPlayers) {
-                if(event.getEntity().getUniqueId().equals(p)) {
-                    // In this case it is a duel and the duel needs to end
-                    Duel d = Main.getDuel(p);
-                    if(d.getPlayer1().getPlayer().equals(p)) {
-                        d.setWinner(d.getPlayer1());
-                    } else {
-                        d.setWinner(d.getPlayer2());
-                    }
-                    d.endMatch(Bukkit.getPlayer(d.getPlayer1().getPlayer()),
-                            Bukkit.getPlayer(d.getPlayer2().getPlayer()));
-                    Main.removeDuel(d);
-
-                }
+        Duel d = Main.getDuel(event.getEntity().getUniqueId());
+        if(d != null) {
+            // The player who died is in a duel.
+            Player p1 = Bukkit.getPlayer(d.getPlayer1().getPlayer());
+            Player p2 = Bukkit.getPlayer(d.getPlayer2().getPlayer());
+            if(p1 != null && event.getEntity().getUniqueId().equals(p1.getUniqueId())) {
+                // Player1 died, player2 wins
+                d.setWinner(d.getPlayer2());
+                d.endMatch(p1, p2);
+            } else if(p2 != null && event.getEntity().getUniqueId().equals(p2.getUniqueId())) {
+                // Player2 died, player1 wins
+                d.setWinner(d.getPlayer1());
+                d.endMatch(p1, p2);
             }
         }
     }
